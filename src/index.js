5b6d0cb3f00a6e0ba443e8de6ea66afb838a22af
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-06-19 18:46:04 +0000

    setup for backend

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT||4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/login', (req, res) => {
+  res.send('login karo')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
