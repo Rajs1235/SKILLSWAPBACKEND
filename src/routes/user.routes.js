5bc7bb38c0095037aef17740c16dfce86db0c93f
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-06-28 02:54:24 +0530

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -43,7 +43,7 @@ const router = express.Router();
 
 // 💥 TEMP: Simplified register route WITHOUT avatar/cover upload
 router.post("/register", registerUser);
-
+router.get('/profile', verifyJWT, getProfileController);
 router.put('/profile', verifyJWT, updateProfileController);
 // keep this — login doesn't need change
 router.post("/login", loginUser);
