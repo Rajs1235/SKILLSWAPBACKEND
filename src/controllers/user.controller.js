86e34286e7034a7d0ceb222101860afab85ebeeb
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-06-28 12:54:12 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -592,17 +592,19 @@ const createConversation = asyncHandler(async (req, res) => {
     user: updatedUser,
   });
 });
- const getProfileController = async (req, res) => {
+export const getProfileController = async (req, res) => {
+  console.log("➡️ /users/profile route hit");
   try {
-    const user = await User.findById(req.user.id).select('fullName userName  learnSkills goals');
+    const user = await User.findById(req.user.id).select("firstName lastName role learnSkills goals");
     if (!user) return res.status(404).json({ message: "User not found" });
 
-    res.status(200).json(user);
+    res.status(200).json({ user });
   } catch (error) {
-    console.error("Error fetching profile:", error);
+    console.error("❌ Error fetching profile:", error);
     res.status(500).json({ message: "Server error" });
   }
 };
+
 export { registerUser,
     loginUser,
     logoutUser,
