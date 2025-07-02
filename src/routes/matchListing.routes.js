10f5762c276acb1248e1ec8a0d454d0fab6677dd
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-07-03 00:10:00 +0530

    Update matchListing.routes.js

--- src/routes/matchListing.routes.js
+++ src/routes/matchListing.routes.js
@@ -1,6 +1,6 @@
 // src/routes/matchListing.routes.js
 import express from "express";
-import { getAllListingsController } from "../user.controller.js";
+import { getAllListingsController } from "../controllers/user.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js"; // or remove if public
 
 const router = express.Router();
