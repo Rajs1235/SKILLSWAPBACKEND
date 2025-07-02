b3736db421f0c37ecf97316178f70fa15e804b7e
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-07-03 00:39:38 +0530

    Update match.routes.js

--- src/routes/match.routes.js
+++ src/routes/match.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-
+  import { verifyJWT } from "../middleware/auth.middleware.js"; 
 import crypto from 'crypto';
 const router = express.Router();
 // Helper to generate consistent room ID between two users
