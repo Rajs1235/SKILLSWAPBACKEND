6e53a2392c9c6c315303bac2fd22d33e3f201061
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-07-03 00:31:09 +0530

    Update matchListing.routes.js

--- src/routes/matchListing.routes.js
+++ src/routes/matchListing.routes.js
@@ -1,11 +1,11 @@
 // src/routes/matchListing.routes.js
 import express from "express";
 import { getAllListingsController } from "../controllers/user.controller.js";
-import { authMiddleware } from "../middleware/auth.middleware.js"; // or remove if public
+import { verifyJWT } from "../middleware/auth.middleware.js"; // or remove if public
 
 const router = express.Router();
 
-router.get("/", authMiddleware, getAllListingsController);
+router.get("/", verifyJWT, getAllListingsController);
 // or: router.get("/", getAllListingsController);
 
 export default router;
