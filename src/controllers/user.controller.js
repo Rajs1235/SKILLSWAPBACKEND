61f60c565b4e5c743cae2c8489a5ad006264e780
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-07-05 16:09:46 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,7 +11,7 @@ import {Conversation} from "../models/conversation.model.js"
 import Message from "../models/message.model.js"
 import {Matches} from "../models/matches.model.js"
 import {Progress} from "../models/progress.model.js"
-import Connection from '../models/connection.model.js';
+
 
 import {TimeTracker} from "../models/timetracker.model.js"
 import crypto from 'crypto';
