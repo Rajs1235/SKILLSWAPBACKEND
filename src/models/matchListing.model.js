f062c743a51683c44aac4667913edfc6338735bc
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-07-05 10:54:14 +0530

    Update matchListing.model.js

--- src/models/matchListing.model.js
+++ src/models/matchListing.model.js
@@ -4,16 +4,8 @@ const matchListingSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    required: true
-  },
-  role: {
-    type: String,
-    enum: ["Tutor", "Learner"],
-    required: true
-  },
-  skills: {
-    type: [String],
-    required: true
+    required: true,
+    unique: true
   }
 }, { timestamps: true });
 
