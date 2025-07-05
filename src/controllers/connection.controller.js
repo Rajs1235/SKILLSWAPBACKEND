7eb0bd30a1d6ecf53cee592c845f2316411bc807
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-07-05 15:11:23 +0530

    Update connection.controller.js

--- src/controllers/connection.controller.js
+++ src/controllers/connection.controller.js
@@ -1,5 +1,5 @@
 // src/controllers/connection.controller.js
-import User from '../models/user.model.js';
+import {User} from '../models/user.model.js';
 
 export const addConnection = async (req, res) => {
   try {
